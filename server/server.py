642d5de689f26aa780308edeb6bdb47f6366f779
Vitor Santos <vitorsacz@gmail.com>
2025-05-02 14:25:24 -0300

    retirando comentários

--- server/server.py
+++ server/server.py
@@ -4,11 +4,10 @@ import os
 
 app = Flask(__name__)
 
-# Função para carregar dados do arquivo JSON
 def carregar_areas_atuacao():
     caminho_json = os.path.join('data', 'areas-atucao.json')
     try:
-        with open(caminho_json, 'r', encoding='utf-8') as arquivo:  # Especifica a codificação utf-8
+        with open(caminho_json, 'r', encoding='utf-8') as arquivo: 
             return json.load(arquivo)
     except Exception as e:
         print(f"Erro ao carregar os dados: {e}")
@@ -17,9 +16,9 @@ def carregar_areas_atuacao():
 @app.route('/areas-atuacao', methods=['GET'])
 def obter_areas():
     dados = carregar_areas_atuacao()
-    if not dados:  # Se não houver dados, retorna um erro 404
+    if not dados: 
         return jsonify({"error": "Dados não encontrados"}), 404
     return jsonify(dados)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)  # Altere a porta se necessário
+    app.run(debug=True, port=5001) 
